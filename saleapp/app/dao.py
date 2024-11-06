83bfdcfb3e78f36762107765e3d57cf419eff151
Thanh Hien <nguyenthithanhhien0604@gmail.com>
2024-11-06 16:27:26 +0700

    sqlalchemy

---
+++ saleapp/app/dao.py
@@ -0,0 +1,9 @@
+from saleapp.app.models import Category,Product
+def load_categories():
+    return Category.query.order_by("id").all()
+
+def load_products():
+    query =Product.query
+    #....
+    return query.all()
+
